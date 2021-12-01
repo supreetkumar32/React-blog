41a8343991a870319dee40bc8367db40b080948f
Supreet <supreetkumar32@gmail.com>
2021-12-01 22:59:15 +0530

    second-commit

--- src/Home.js
+++ src/Home.js
@@ -68,32 +68,59 @@
 
 
 /////////////////////////////////////////////////////
-import { useState,useEffect } from "react";
+// import { useState,useEffect } from "react";
+// import BlogList from "./BlogList";
+
+// const Home = () => {
+//   const [blogs, setBlogs] = useState([
+//     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+//     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+//     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+//   ])
+//   const handleDelete = (id) => {
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(newBlogs);
+//   }
+   
+//   const[name,setName]=useState('mario');
+//   useEffect(()=>{
+//       console.log('use effect ran');
+//       console.log(name);
+//   },[name]);
+//   return (
+//     <div className="home">
+//       {/* <BlogList blogs={blogs} title="All Blogs" /> */}
+//       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+//       {/* <BlogList blogs={blogs.filter((blogz)=>blogz.author==='mario')} title="Mario's Blogsz" /> */}
+//       <button onClick={()=>setName('luigi')}>change name</button>
+//       <p>{name}</p>
+//     </div>
+//   );
+// }
+ 
+// export default Home;
+
+////////////////////////////////////
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-   
-  const[name,setName]=useState('mario');
-  useEffect(()=>{
-      console.log('use effect ran');
-      console.log(name);
-  },[name]);
+  const [blogs, setBlogs] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        //console.log(data);
+      })
+  }, [])
+
   return (
     <div className="home">
-      {/* <BlogList blogs={blogs} title="All Blogs" /> */}
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      {/* <BlogList blogs={blogs.filter((blogz)=>blogz.author==='mario')} title="Mario's Blogsz" /> */}
-      <button onClick={()=>setName('luigi')}>change name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
